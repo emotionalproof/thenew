485a6db3c7db24893f2cecf21c9ede6aef722899
chloekim917 <chloekim917@gmail.com>
2020-06-15 17:43:15 -0400

    crud for action_post

--- app/controllers/action_posts_controller.rb
+++ app/controllers/action_posts_controller.rb
@@ -1,3 +1,42 @@
 class ActionPostsController < ApplicationController
+    before_action :find_action_post, only:[:show, :edit, :update, :destroy]
+    def index
+        @action_posts = ActionPost.all
+    end
 
+    def show
+    end
+
+    def new
+        @action_post = ActionPost.new
+    end
+
+    def create
+        action_post = ActionPost.create(action_post_params)
+        redirect_to action_post_path(action_post.id)
+    end
+
+    def edit
+    end
+
+    def update
+        @action_post.update(action_post_params)
+        redirect_to action_post_path(@action_post.id)
+    end
+
+    def destroy
+        @action_post.def destroy
+            redirect_to action_posts_path
+        end
+    end
+
+    private
+
+    def action_post_params
+        params.require(:action_post).permit(:title, :content, :location, :category_id, :img_url)
+    end
+
+    def find_action_post
+        @action_post = ActionPost.find(params[:id])
+    end
 end
