1b09863fbf5f20f64ad974a78b676254ef897610
chloekim917 <chloekim917@gmail.com>
2020-06-15 17:12:45 -0400

    delete duplicated routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   resources :messages
   resources :follows
   resources :comments
-  resources :categories
-  resources :action_posts
+  resources :action_posts, only:[:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :action_posts, only:[:index, :show, :new, :create, :edit, :update, :destroy]
+  
 end
