d5ea594b906c7fbd4b36358be5f093d5da403e79
chloekim917 <chloekim917@gmail.com>
2020-06-15 18:12:04 -0400

    preliminary user controller & view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
+    before_action :find_user, only:[:show, :edit, :update]
+    def show
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.create(user_params)
+        redirect_to user_path(user.id)
+    end
+
+    def edit
+    end
+
+    def update
+        @user.update(user_params)
+        redirect_to user_path(@user.id)
+    end
+
+    private
     
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password, :img_url, :img_token, :user_name)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only:[:show, :new, :edit, :update]
   resources :user_messages
   resources :user_categories
   resources :messages
   resources :follows
   resources :comments
   resources :action_posts, only:[:index, :show, :new, :create, :edit, :update, :destroy]
+
+  get '/users/:id/', to "users"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   
